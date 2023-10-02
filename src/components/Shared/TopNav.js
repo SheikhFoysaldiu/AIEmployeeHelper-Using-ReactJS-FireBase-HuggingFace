d1a1418f20dfb2bcfd66fc3449fa9a5c53ae4dc8
SheikhFoysaldiu <foysal15-3442@diu.edu.bd>
2023-10-02 12:22:27 +0600

    Fix

--- src/components/Shared/TopNav.js
+++ src/components/Shared/TopNav.js
@@ -9,11 +9,11 @@ const TopNav = () => {
 
   return (
     <section className="max-w-6xl w-full mx-auto my-0 px-4 md:py-9">
-      <div className="hidden md:flex md:items-center md:justify-between">
-        <div className="">
-          <h1 className="text-xl font-semibold">AIEmployeeHelper</h1>
+      <div className="md:flex md:items-center md:justify-between">
+        <div className="hidden md:block">
+          <h1 className="md:text-xl font-semibold">AIEmployeeHelper</h1>
         </div>
-        <div className="flex gap-8 py-2">
+        <div className="flex justify-end gap-8 py-2">
           <Link to="/">
             <button className="text-btnNavColor hover:text-btnHoverColor">
               Home
